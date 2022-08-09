3282a0d165bf8dbefaa376d5fab81d0cc724c794
Walid Jamili <wal.jam21@gmail.com>
2022-08-09 14:50:38 +0200

    player movements modified + field limits defined

--- bgdr.js
+++ bgdr.js
@@ -1,139 +1,127 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = 1280;
+canvas.height = 1024;
 
 class Player {
-  constructor({ position, velocity }) {
+  constructor({ position, velocity, keys }) {
     this.position = position;
     this.velocity = velocity;
     this.height = 500;
+    this.width = 80;
     this.gravity = 9.81;
+    this.keys = keys;
   }
   draw() {
     ctx.fillStyle = "red";
-    ctx.fillRect(this.position.x, this.position.y, 50, this.height);
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
-  drop() {
-    this.draw();
+  move() {
+    if (this.position.x + this.velocity.x < 0) {
+      this.position.x = 0;
+      this.velocity.x = 0;
+      console.log("iamout");
+    } else if (this.position.x + this.velocity.x + this.width >= canvas.width) {
+      this.position.x = canvas.width - this.width;
+      this.velocity.x = 0;
+      console.log("iamout");
+    } else {
+      this.position.x += this.velocity.x;
+    }
+
     this.position.y += this.velocity.y;
-    this.position.x += this.velocity.x;
+
     if (this.position.y + this.height + this.velocity.y >= canvas.height - 100) {
       this.velocity.y = 0;
     } else {
       this.velocity.y += this.gravity;
     }
+    this.draw();
   }
-  move() {
-    window.addEventListener("keydown", (keys) => {
-      console.log(keys.key);
-      switch (keys.key) {
-        case "q":
-          this.velocity.x = -20;
-          break;
-        case "z":
-          this.velocity.y = -70;
-          break;
-        case "s":
-          this.height = 222;
-          break;
-        case "d":
-          this.velocity.x = 20;
-          break;
-        default:
-          this.position.x += 0;
-          this.position.y += 0;
-          this.velocity.x += 0;
-          this.velocity.y += 0;
-      }
-    });
-    window.addEventListener("keyup", (keys) => {
-      switch (keys.key) {
-        case "s":
-          this.position.y -= 500;
-          this.height = 500;
-
-          break;
-        case "q":
-        case "d":
-          this.velocity.x = 0;
-          break;
-        case "z":
-          this.velocity.y = 60;
-          this.position.y -= 60;
-      }
-    });
+  setSpeed(key) {
+    switch (key) {
+      case this.keys[0]:
+      case this.keys[4]:
+        this.velocity.x = -20;
+        break;
+      case this.keys[1]:
+      case this.keys[5]:
+        this.velocity.y = -70;
+        break;
+      case this.keys[3]:
+      case this.keys[7]:
+        this.height = 222;
+        break;
+      case this.keys[2]:
+      case this.keys[6]:
+        this.velocity.x = 20;
+        break;
+      default:
+        break;
+    }
   }
-  move2() {
-    window.addEventListener("keydown", (keys) => {
-      console.log(keys.key);
-      switch (keys.key) {
-        case "j":
-          this.velocity.x = -20;
-          break;
-        case "i":
-          this.velocity.y = -60;
-          break;
-        case "k":
-          this.height = 222;
-          break;
-        case "l":
-          this.velocity.x = 20;
-          break;
-        default:
-          this.position.x += 0;
-          this.position.y += 0;
-          this.velocity.x += 0;
-          this.velocity.y += 0;
-      }
-    });
-    window.addEventListener("keyup", (keys) => {
-      switch (keys.key) {
-        case "k":
-          this.position.y -= 500;
-          this.height = 500;
+  stop(key) {
+    switch (key) {
+      case this.keys[3]:
+      case this.keys[7]:
+        this.position.y -= 500;
+        this.height = 500;
 
-          break;
-        case "j":
-        case "l":
-          this.velocity.x = 0;
-          break;
-        case "i":
-          this.velocity.y = 60;
-          this.position.y -= 60;
-      }
-    });
+        break;
+      case this.keys[0]:
+      case this.keys[2]:
+      case this.keys[4]:
+      case this.keys[6]:
+        this.velocity.x = 0;
+        break;
+      case this.keys[1]:
+      case this.keys[5]:
+        this.velocity.y = 60;
+        this.position.y -= 60;
+    }
   }
 }
 
 const player1 = new Player({
   position: {
-    x: 400,
+    x: 320,
     y: 0,
   },
   velocity: {
     x: 0,
     y: 0,
   },
+  keys: ["q", "z", "d", "s"],
 });
 
 const player2 = new Player({
   position: {
-    x: 1450,
+    x: 960,
     y: 0,
   },
   velocity: {
     x: 0,
     y: 0,
   },
+  keys: ["j", "i", "l", "k"],
 });
 
 function gameStart() {
   window.requestAnimationFrame(gameStart);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  player1.drop();
-  player2.drop();
+  player1.move();
+  player2.move();
 }
-player1.move();
-player2.move2();
+
 gameStart();
+
+window.addEventListener("keydown", (event) => {
+  console.log(event.key);
+  player1.setSpeed(event.key);
+  player2.setSpeed(event.key);
+});
+
+window.addEventListener("keyup", (event) => {
+  player1.stop(event.key);
+  player2.stop(event.key);
+});
